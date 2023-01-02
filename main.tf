d6409c392e64ff09e5e5f5e34b36207ef5dbd1cb
Smalbach <smalbach24@gmail.com>
2023-01-02 11:52:26 -0500

    feat: load balancer

--- main.tf
+++ main.tf
@@ -13,24 +13,63 @@ data "aws_vpc" "default" {
   default = true
 }
 
+# ----------------------------------------------------
+# Data Source to get the default subnet ID
+# ----------------------------------------------------
+data "aws_subnet" "az_a" {
+  availability_zone = "us-east-1a"
+  #if you have more than one subnet in the same AZ, you can use the default_for_az attribute to get the default subnet for the AZ
+  default_for_az              =  true
+}
+data "aws_subnet" "az_b" {
+  availability_zone = "us-east-1b"
+  #if you have more than one subnet in the same AZ, you can use the default_for_az attribute to get the default subnet for the AZ
+  default_for_az              =  true
+}
 
 # ---------------------------------------
 # Define AMI instance: Ububtu  -> https://cloud-images.ubuntu.com/locator/ec2/
+# server-1: web server
 # ---------------------------------------
-resource "aws_instance" "my_server" {
+resource "aws_instance" "server-1" {
   ami           = "ami-0a6b2839d44d781b2"
   instance_type = "t2.micro"
+  subnet_id = data.aws_subnet.az_a.id
   vpc_security_group_ids = [ aws_security_group.security_group.id ]
   
   // Crete a file with the user data
   // used to install the web server and start it
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "Hello, World- server 1" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
+    tags = {
+    Name = "server-1"
+  }
 }
 
+
+# ---------------------------------------
+# Define server-2: web server
+# ---------------------------------------
+resource "aws_instance" "server-2" {
+  ami           = "ami-0a6b2839d44d781b2"
+  instance_type = "t2.micro"
+  subnet_id = data.aws_subnet.az_b.id
+  vpc_security_group_ids = [ aws_security_group.security_group.id ]
+  
+  // Crete a file with the user data
+  // used to install the web server and start it
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World - server 2" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+    tags = {
+    Name = "server-2"
+  }
+}
 
 # ---------------------------------------
 # Define Security Group
@@ -40,11 +79,97 @@ resource "aws_security_group" "security_group" {
   description = "Allow HTTP traffic"
   vpc_id = data.aws_vpc.default.id
   ingress {
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb.id]
     description = "HTTP"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
   }
 
+}
+
+# ---------------------------------------
+# Define Load Balancer
+# ---------------------------------------
+resource "aws_lb" "alb" {
+  load_balancer_type  = "application"
+  name                = "terraformers-alb"
+  security_groups     = [aws_security_group.alb.id]
+  subnets             = [data.aws_subnet.az_a.id, data.aws_subnet.az_b.id]
+}
+
+
+# ---------------------------------------
+# Define Security Group for Load Balancer
+# ---------------------------------------
+resource "aws_security_group" "alb" {
+  name          = "alb-sg"
+  description   = "Allow  port 80  HTTPtraffic"
+  vpc_id        = data.aws_vpc.default.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+  }
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+  }
+
+}
+
+
+# ---------------------------------------
+# Define Target Group of Load Balancer
+# ---------------------------------------
+resource "aws_lb_target_group" "this" {
+  name                  = "alb-tg"
+  port                  = 80
+  protocol              = "HTTP"
+  vpc_id                = data.aws_vpc.default.id
+
+  health_check {
+    enabled             = true
+    matcher             = "200"
+    path                = "/"
+    port                = "8080"
+    protocol            = "HTTP"
+  }
+
+}
+
+# ---------------------------------------
+# Define Listener of Load Balancer server 1
+# ---------------------------------------
+resource "aws_lb_target_group_attachment" "server-1" {
+  target_group_arn = aws_lb_target_group.this.arn
+  target_id        = aws_instance.server-1.id
+  port             = 8080
+}
+
+
+# ---------------------------------------
+# Define Listener of Load Balancer server 2
+# ---------------------------------------
+resource "aws_lb_target_group_attachment" "server-2" {
+  target_group_arn = aws_lb_target_group.this.arn
+  target_id        = aws_instance.server-2.id
+  port             = 8080
+}
+
+# ---------------------------------------
+# Define Listener of Load Balancer
+# ---------------------------------------
+resource "aws_lb_listener" "this" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,43 @@
 #---------------------------------------
-# get the public dns of the instance
+# get the public dns of the instance server 1
 #---------------------------------------
-output "public_dns" {
+output "public_dns_server_1" {
   description = "value of the public dns"
-  value = "http://${aws_instance.my_server.public_dns}:8080"
+  value = "http://${aws_instance.server-1.public_dns}:8080"
 }
 
 #---------------------------------------
-# get the public ipv4 of the instance
+# get the public ipv4 of the instance server 1
 #---------------------------------------
-output "public_ip" {
+output "public_ip_server_1" {
   description = "value of the public ip"
-  value = "http://${aws_instance.my_server.public_ip}:8080"
+  value = "http://${aws_instance.server-1.public_ip}:8080"
+}
+
+
+
+#---------------------------------------
+# get the public dns of the instance server 2
+#---------------------------------------
+output "public_dns_server_2" {
+  description = "value of the public dns"
+  value = "http://${aws_instance.server-2.public_dns}:8080"
+}
+
+#---------------------------------------
+# get the public ipv4 of the instance server 2
+#---------------------------------------
+output "public_ip_server_2" {
+  description = "value of the public ip"
+  value = "http://${aws_instance.server-2.public_ip}:8080"
+}
+
+
+
+#---------------------------------------
+# get the public dns of the instance load balancer
+#---------------------------------------
+output "public_dns_load_balancer" {
+  description = "value of the public dns load balancer"
+  value = "http://${aws_lb.alb.dns_name}"
 }
