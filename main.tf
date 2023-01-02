10938afcae9bf2a5fa1dec032e3374cb719d6441
Smalbach <smalbach24@gmail.com>
2023-01-02 09:05:53 -0500

    feact: create an ec2 instance

--- main.tf
+++ main.tf
@@ -1,16 +1,29 @@
+
+# -------------------------
+# Define aws provider
+# -------------------------
 provider "aws" {
   region = "us-east-1"
 }
 
+# ----------------------------------------------------
+# Data Source to get the default VPC ID 
+# ----------------------------------------------------
 data "aws_vpc" "default" {
   default = true
 }
 
 
+# ---------------------------------------
+# Define AMI instance: Ububtu  -> https://cloud-images.ubuntu.com/locator/ec2/
+# ---------------------------------------
 resource "aws_instance" "my_server" {
   ami           = "ami-0a6b2839d44d781b2"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.security_group.id ]
+  
+  // Crete a file with the user data
+  // used to install the web server and start it
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
@@ -18,6 +31,10 @@ resource "aws_instance" "my_server" {
               EOF
 }
 
+
+# ---------------------------------------
+# Define Security Group
+# ---------------------------------------
 resource "aws_security_group" "security_group" {
   name        = "my-sg"
   description = "Allow HTTP traffic"
